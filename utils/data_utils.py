13516a3b166a8475a97818c210edbab0b6a48d29
Tymoteusz Cejrowski <tymtoeusz.cejrowski@smartula.eu>
2021-08-26 13:50:31 +0200

    Added support for pathlin in datautils for datetime filtering.

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -12,6 +12,8 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from typing import Callable
 from datetime import datetime
 from tqdm import tqdm
+from pathlib import Path
+from typing import List
 
 
 def flatten(x):
@@ -64,40 +66,42 @@ def create_valid_sounds_datalist(root_folder, valid_file_filename='valid_sounds.
     return valid_dict_count
 
 
-def get_valid_sounds_datalist(folder_list, valid_file_filename):
-    """Reads valid sounds files in specific directories. Note that files should exists, 
+def get_valid_sounds_datalist(folder_list: List[Path], valid_file_filename: str = 'valid_sounds.txt'):
+    """
+    Reads valid sounds files in specific directories. Note that files should exists,
     see create_valid_sounds_datalis method
-    
-    Parameters:
-        folder_list (str): list with folder which will be scanned
-        valid_file_filename (str): filename which will be read from folder_list
-    
-    Returns:
-
+    :param folder_list: list of folders which should be scanned
+    :param valid_file_filename: filename for 'valid' sound filenames file.
+    :return: list of
     """
     sound_filenames = []
 
     for folder in folder_list:
-        summary_file = os.path.join(folder, valid_file_filename)
-        if os.path.isfile(summary_file):
-            with open(summary_file, 'r') as f:
-                sound_filenames += list(map(lambda x: os.path.join(folder, x), f.read().splitlines()))
+        summary_file = folder / valid_file_filename
+        if summary_file.exists():
+            with summary_file.open('r') as f:
+                sound_filenames += list(map(lambda x: folder / x, f.read().splitlines()))
         else:
             logging.warning(f'{valid_file_filename} for folder {folder} does not exists! skipping')
 
     return sound_filenames
 
 
-def filter_by_datetime(str_list, start, end):
-    """ Function for filtering sound samples names by date time """
-
-    def _is_str_indatetimerage(elem):
-        """ closure for filter_bydatetiem """
-        elem = "-".join(elem.split(os.sep)[-1].split('-')[1:]).split('.')[0]
+def filter_by_datetime(files: List[Path], start: datetime, end: datetime):
+    """
+    Filtering list of Path based on their datetime encoded within filename
+    :param files:   list of sound files paths which should be filtered
+    :param start:   start datetime
+    :param end:     end datetime
+    :return: list of paths
+    """
+    def _is_within_timerange(elem):
+        """ predicate performing filename datetime parsing and checking timerange """
+        elem = "-".join(elem.stem.split('-')[1:]).split('.')[0]
         datetime_elem = datetime.strptime(elem, '%Y-%m-%dT%H-%M-%S')
         return start <= datetime_elem <= end
 
-    return list(filter(_is_str_indatetimerage, str_list))
+    return list(filter(_is_within_timerange, files))
 
 
 def filter_string_list(input_str_list, *names):
