c910aa4264103554551d49f5eaebce496a72d5a6
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2021-09-09 13:10:57 +0200

    Feature/support for docker container training (#8)
    * added dockerignore to exclude possible data folder.
    * added improvements for handling different pytorch/docker cuda versions.
    * added some more verbose logging on startup script.
    
    Co-authored-by: Tymoteusz Cejrowski <tymtoeusz.cejrowski@smartula.eu>

--- build_docker.sh
+++ build_docker.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
 echo "-> Running build docker image script..."
-CUDA_VERSION=$(nvcc --version | sed -n 's/^.*release \([0-9\.]\+\).*$/\1/p')
-echo "-> Your device has cuda:$CUDA_VERSION"
+HOST_CUDA_VERSION=$(nvcc --version | sed -n 's/^.*release \([0-9\.]\+\).*$/\1/p')
+echo "-> Running on host with cuda:$HOST_CUDA_VERSION"
 
-PYTORCH_CUDA_VERSION=$CUDA_VERSION
-DOCKER_CUDA_VERSION=$CUDA_VERSION
+PYTORCH_CUDA_VERSION=HOST_CUDA_VERSION
+DOCKER_CUDA_VERSION=HOST_CUDA_VERSION
 
 IFS='.'
-read -a strarr <<< "$CUDA_VERSION"
+read -a strarr <<< "$HOST_CUDA_VERSION"
 if [ $((strarr[0])) -gt 10 ]
-then
+  then
   PYTORCH_CUDA_VERSION=11.1
-  DOCKER_CUDA_VERSION=$CUDA_VERSION.0
+  if [ $((strarr[0])) -gt 11 ]
+    then
+    DOCKER_CUDA_VERSION=$HOST_CUDA_VERSION.0
+  else
+    DOCKER_CUDA_VERSION=$HOST_CUDA_VERSION.1
+  fi
 else
   PYTORCH_CUDA_VERSION=10.2
 fi
 
+while getopts ":d:p:" opt; do
+  case $opt in
+    d) DOCKER_CUDA_VERSION="$OPTARG"
+       echo "-> overwriting host DOCKER cuda version with value $OPTARG"
+    ;;
+    p) PYTORCH_CUDA_VERSION="$OPTARG"
+      echo "-> overwriting host PYTORCH cuda version with value $OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
 echo "-> Using pytorch cudatoolkit of version $PYTORCH_CUDA_VERSION"
-if [ "$PYTORCH_CUDA_VERSION" != "$CUDA_VERSION" ]
+if [ "$PYTORCH_CUDA_VERSION" != "$HOST_CUDA_VERSION" ] || [ "$PYTORCH_CUDA_VERSION" != "$DOCKER_CUDA_VERSION" ]
 then
   echo "-> ! WARNING ! possible mismatch for local cuda version and pytorch installer!"
+  echo "     pytorch_v($PYTORCH_CUDA_VERSION) / docker_cuda_v($DOCKER_CUDA_VERSION) / cuda_host_v($HOST_CUDA_VERSION)"
 fi
 
 cp environment.yml environment_backup.yml
 awk -v cuda_ver="$PYTORCH_CUDA_VERSION" '{sub(/\${CUDA_TOOLKIT_VERSION}/, cuda_ver)}1' environment_backup.yml > environment.yml
-docker build -t buzz-based-anomaly . --build-arg CUDA_VERSION="$DOCKER_CUDA_VERSION"
+docker build -t "buzz-based-anomaly:cuda-$PYTORCH_CUDA_VERSION" . --build-arg CUDA_VERSION="$DOCKER_CUDA_VERSION"
 mv environment_backup.yml environment.yml
