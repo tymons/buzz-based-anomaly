fcf7dac75996396a210e87e26ae42ad251d9fff5
Tymoteusz Cejrowski <tymtoeusz.cejrowski@smartula.eu>
2021-09-22 14:17:36 +0200

    Added more tests.

--- test/cond2d_ae_model_test.py
+++ test/cond2d_ae_model_test.py
@@ -40,6 +40,74 @@ class Conv2DAeModelTest(unittest.TestCase):
         self.assertEqual(config['max_pool'], model.get_params().get('model_max_pool'))
         self.assertEqual(input_size, model(torch.empty(size=(1, 1, *input_size))).shape[2:])
 
+    def test_model_is_build_different_input_size(self):
+        config, _ = get_default_config()
+        input_size = (175, 4523)
+
+        model: BaseModel = HiveModelFactory.build_model(HiveModelType.from_name('conv2d_autoencoder'), input_size,
+                                                        config)
+
+        self.assertIsNotNone(model, "model build failed!")
+        self.assertIsNotNone(model_check(model, (1, *input_size), device='cpu'), "model verification failed!")
+        self.assertListEqual(config['layers'], model.get_params().get('model_feature_map'))
+        self.assertListEqual(config['dropout'], model.get_params().get('model_dropouts'))
+        self.assertEqual(config['kernel'], model.get_params().get('model_kernel_size'))
+        self.assertEqual(config['padding'], model.get_params().get('model_padding'))
+        self.assertEqual(config['latent'], model.get_params().get('model_latent'))
+        self.assertEqual(config['max_pool'], model.get_params().get('model_max_pool'))
+        self.assertEqual(input_size, model(torch.empty(size=(1, 1, *input_size))).shape[2:])
+
+    def test_model_is_build_bigger_max_pool(self):
+        config, input_size = get_default_config()
+        config['max_pool'] = 5
+
+        model: BaseModel = HiveModelFactory.build_model(HiveModelType.from_name('conv2d_autoencoder'), input_size,
+                                                        config)
+
+        self.assertIsNotNone(model, "model build failed!")
+        self.assertIsNotNone(model_check(model, (1, *input_size), device='cpu'), "model verification failed!")
+        self.assertListEqual(config['layers'], model.get_params().get('model_feature_map'))
+        self.assertListEqual(config['dropout'], model.get_params().get('model_dropouts'))
+        self.assertEqual(config['kernel'], model.get_params().get('model_kernel_size'))
+        self.assertEqual(config['padding'], model.get_params().get('model_padding'))
+        self.assertEqual(config['latent'], model.get_params().get('model_latent'))
+        self.assertEqual(config['max_pool'], model.get_params().get('model_max_pool'))
+        self.assertEqual(input_size, model(torch.empty(size=(1, 1, *input_size))).shape[2:])
+
+    def test_model_is_build_different_padding(self):
+        config, input_size = get_default_config()
+        config['padding'] = 4
+
+        model: BaseModel = HiveModelFactory.build_model(HiveModelType.from_name('conv2d_autoencoder'), input_size,
+                                                        config)
+
+        self.assertIsNotNone(model, "model build failed!")
+        self.assertIsNotNone(model_check(model, (1, *input_size), device='cpu'), "model verification failed!")
+        self.assertListEqual(config['layers'], model.get_params().get('model_feature_map'))
+        self.assertListEqual(config['dropout'], model.get_params().get('model_dropouts'))
+        self.assertEqual(config['kernel'], model.get_params().get('model_kernel_size'))
+        self.assertEqual(config['padding'], model.get_params().get('model_padding'))
+        self.assertEqual(config['latent'], model.get_params().get('model_latent'))
+        self.assertEqual(config['max_pool'], model.get_params().get('model_max_pool'))
+        self.assertEqual(input_size, model(torch.empty(size=(1, 1, *input_size))).shape[2:])
+
+    def test_model_is_build_different_kernel(self):
+        config, input_size = get_default_config()
+        config['kernel'] = 6
+
+        model: BaseModel = HiveModelFactory.build_model(HiveModelType.from_name('conv2d_autoencoder'), input_size,
+                                                        config)
+
+        self.assertIsNotNone(model, "model build failed!")
+        self.assertIsNotNone(model_check(model, (1, *input_size), device='cpu'), "model verification failed!")
+        self.assertListEqual(config['layers'], model.get_params().get('model_feature_map'))
+        self.assertListEqual(config['dropout'], model.get_params().get('model_dropouts'))
+        self.assertEqual(config['kernel'], model.get_params().get('model_kernel_size'))
+        self.assertEqual(config['padding'], model.get_params().get('model_padding'))
+        self.assertEqual(config['latent'], model.get_params().get('model_latent'))
+        self.assertEqual(config['max_pool'], model.get_params().get('model_max_pool'))
+        self.assertEqual(input_size, model(torch.empty(size=(1, 1, *input_size))).shape[2:])
+
 
 if __name__ == '__main__':
     unittest.main()
