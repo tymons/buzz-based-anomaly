9d89f7dc2e05072c15e9dcba4cf1f0032edae949
Tymoteusz Cejrowski <tymtoeusz.cejrowski@smartula.eu>
2021-09-29 19:00:38 +0200

    Test refactored. Files moved to different folder.

--- test/features/spectrogram_feature_test.py
+++ test/features/spectrogram_feature_test.py
@@ -27,8 +27,8 @@ def _get_default_config() -> dict:
 
 class TestPeriodogramMethods(unittest.TestCase):
     def setUp(self):
-        self.filename_10kHz = Path(Path(__file__).parent.resolve(), 'data/10kHz.wav')
-        self.filename_1kHz = Path(Path(__file__).parent.resolve(), 'data/1kHz.wav')
+        self.filename_10kHz = Path(Path(__file__).parent.resolve(), '../data/10kHz.wav')
+        self.filename_1kHz = Path(Path(__file__).parent.resolve(), '../data/1kHz.wav')
 
     def test_10kHz_basic(self):
         config = _get_default_config()
