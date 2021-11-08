f4c6fedf71be886595c34fa2bfaf27483d02e3a0
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2021-11-08 11:25:55 +0100

    Feature/buzz based fingerprint filtering support (#57)
    * Added support for mergning sound feature file with smartula weather.
    * Added enum class for defining weather feature type

--- utils/side_scripts/weather.py
+++ utils/side_scripts/weather.py
@@ -1,36 +1,55 @@
-import requests
 import pandas as pd
 import argparse
+import pytz
 
 from pathlib import Path
-
-OPEN_WEATHER_API = 'http://history.openweathermap.org/data/2.5/history/city'
-
-
-def get_temperature_humidity_openweather(x, api_key, lat, long):
-    """ Function for making call to openeather api """
-    start_utc = int((x['datetime'] - pd.Timedelta(hours=2)).timestamp())
-    print(f'making api call for: {start_utc}...', end=' ')
-
-    payload = {'lat': str(lat), 'lon': str(long), 'type': 'hour', 'appid': api_key, 'start': str(start_utc), 'cnt': '1'}
-    r = requests.get(OPEN_WEATHER_API, params=payload)
-    if r.status_code == 200:
-        print('success!')
-        r_json = r.json()
-        x['outdoor_temperature'] = round(r_json['list'][0]['main']['temp'] - 273.15, 2)
-        x['outdoor_humidity'] = round(r_json['list'][0]['main']['humidity'], 2)
-        return x
-    else:
-        raise ValueError(f"Error at http call: {r.text}")
+from weather_feature_type import WeatherFeatureType
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Fulfill missing values in csv feature file')
-    parser.add_argument('file', metavar='csv_file', default='feature.csv', type=Path, help='csv feature file')
-    parser.add_argument('latitude', default='64.308498766', metavar='latitude', type=float, help='location latitude')
-    parser.add_argument('longitude', default='28.468998124', metavar='longitude', type=float, help='location latitude')
-    parser.add_argument('key', metavar='key', default='deadbeef', type=str, help='open weather api key')
+    parser = argparse.ArgumentParser(description='Merge csv feature with thermal values')
+    parser.add_argument('--feature-file', metavar='feature_file', default='feature.csv',
+                        type=Path, help='csv feature file')
+    parser.add_argument('--weather-file', metavar='weather_file', default='weather.csv',
+                        type=Path, help='csv weather file')
+    parser.add_argument('--output-file', metavar='output_file', default='output.csv',
+                        type=Path, help='csv output merged file')
+    parser.add_argument('--open-weather-file', metavar='weather_file', default='open-weather.csv',
+                        type=Path, help='csv open weather file')
+    parser.add_argument('--weather-file-feature-name', default=WeatherFeatureType.TEMPERATURE,
+                        choices=list(WeatherFeatureType), type=WeatherFeatureType.from_name,
+                        help='feature name to be used with weather csv file')
+    parser.add_argument('--open-weather-file-feature-name', default=WeatherFeatureType.TEMPERATURE,
+                        choices=list(WeatherFeatureType), type=WeatherFeatureType.from_name,
+                        help='feature name to be used with open weather csv file')
+    parser.add_argument('--timezone', metavar='timezone', type=pytz.timezone, default='UTC',
+                        help="timezone to be applied to concatenated dataframe")
+
+    parser.set_defaults(open_weather=False)
     args = parser.parse_args()
+
+    df_weather = pd.read_csv(args.weather_file, usecols=['datetime', args.weather_file_feature_name])
+    df_feature = pd.read_csv(args.feature_file, usecols=['datetime', 'hive', 'feature'])
+    df_weather['datetime'] = pd.to_datetime(df_weather['datetime'], utc=True)
+    df_feature['datetime'] = pd.to_datetime(df_feature['datetime'], utc=True)
+    df_feature.sort_values('datetime', inplace=True)
+    df_weather.sort_values('datetime', inplace=True)
+
+    df_weather_reindex = df_weather.set_index('datetime').reindex(df_feature.set_index('datetime').index,
+                                                                  method='nearest', tolerance='50min').reset_index()
+
+    data = pd.merge(df_feature, df_weather_reindex, on='datetime').drop_duplicates()
+    data['datetime'] = data['datetime'].dt.tz_convert(args.timezone)
+
+    if data[args.weather_feature_name].isna().sum() > 0:
+        print(f'got some missing data on {args.weather_file_feature_name} with len '
+              f'{data[args.weather_file_feature_name].isna().sum()}/{data[args.weather_file_feature_name].count()}'
+              f' updating with open weather file...')
+        # TODO implement
+        return
+
+    print(f'saving data to file {args.output_file}...')
+    data.to_csv(args.output_file, index=False)
 
 
 if __name__ == "__main__":
