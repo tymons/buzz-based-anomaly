1989a71b5e972301e93447bdf8f09c4674a96898
tymons <tymoteusz.cejrowski@gmail.com>
2021-12-21 12:26:23 +0100

    Fixing flake8 warnings.

--- models/vanilla/conv2d_ae.py
+++ models/vanilla/conv2d_ae.py
@@ -107,8 +107,8 @@ class Conv2DDecoder(nn.Module):
 
 
 class Conv2DAE(BaseModel):
-    def __init__(self, model_type: HiveModelType, features: List[int], dropout_probs: List[float], kernel_size: int, padding: int, max_pool: int,
-                 latent: int, input_size: tuple):
+    def __init__(self, model_type: HiveModelType, features: List[int], dropout_probs: List[float], kernel_size: int,
+                 padding: int, max_pool: int, latent: int, input_size: tuple):
         super().__init__(model_type)
         self._feature_map = features
         self._dropout_probs = dropout_probs
