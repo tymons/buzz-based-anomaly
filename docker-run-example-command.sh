474b58c30ff72519f9ffb2cad034ccf973579613
tymons <tymoteusz.cejrowski@gmail.com>
2021-12-28 19:42:06 +0100

    Updating docker run example command with ability to pick up gpus.

--- docker-run-example-command.sh
+++ docker-run-example-command.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-docker run -d --name buzz-based-anomaly \
+docker run -d --name buzz-based-anomaly-contrastive95-ae \
+-e CUDA_VISIBLE_DEVICES=1 \
 -v /home/tcejrowski/buzz-based-anomaly-dataset/smartula:/data \
 -v /home/macierz/142847ct/142847ct/research/buzz-based-anomaly:/io \
 --gpus all \
--t tymonzz/buzz-based-anomaly:cuda-11.1.1 \
-autoencoder periodogram /data \
---hives DEADBEEF93 \
+-t tymonzz/buzz-based-anomaly:cuda-11.1.1-pytorch-mel \
+contrastive_conv2d_vae melspectrogram /data \
+--hives DEADBEEF95 \
+--contrastive_data_folder \
+/data/DEADBEEF94 /data/DEADBEEF93 /data/DEADBEEF97 \
+--use_contrastive_valid_file \
 --filter_dates 2020-08-10 2021-08-10 \
 --log_folder /io/output/logs \
 --model_output /io/output/models \
---model_config /io/input/model_config.yml \
---feature_config /io/input/feature_config.yml \
+--model_config /io/input/model_config_conv2d.yml \
+--feature_config /io/input/feature_config_melspectrogram.yml \
 --learning_config /io/input/learning_config.yml \
---comet_config /io/input/comet.config
+--comet_config /io/input/comet.config \
+--gpu_ids 0
